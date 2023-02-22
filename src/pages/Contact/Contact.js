6d7f43e4dd3104fc9ace9de776970377e5712f6b
lolex <apazahuaychohugoalejandro@gmail.com>
2023-02-21 21:44:31 -0400

    prueba firebae

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -8,11 +8,25 @@ import {
   StyledSubmitButton
 } from "./StyledContact";
 
+import { auth } from '../../services/firebase';
+
 const ContactPage = () => {
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
     alert("Form may or may not have been submitted :)");
+
+    auth().signInWithEmailAndPassword(email, password)
+      .then(() => {
+        alert("Inicio de sesión exitoso");
+        e.target.reset();
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
 
     e.target.reset();
   }
@@ -22,7 +36,20 @@ const ContactPage = () => {
       <PageHeader title="Contact" />
       <section>
         <StyledForm onSubmit={handleFormSubmit}>
+
           <StyledLabel>
+            E-mail:
+            <StyledInput type="email" name="email" required />
+          </StyledLabel>
+
+          <StyledLabel>
+            Contraseña:
+            <StyledInput type="password" name="password" required />
+          </StyledLabel>
+
+          <StyledSubmitButton type="submit" value="Iniciar sesión" />
+
+          {/* <StyledLabel>
             Name:
             <StyledInput type="text "name="name" required />
           </StyledLabel>
@@ -37,7 +64,7 @@ const ContactPage = () => {
             <StyledTextArea name="message" required />
           </StyledLabel>
 
-          <StyledSubmitButton type="submit" value="Send" />
+          <StyledSubmitButton type="submit" value="Send" /> */}
         </StyledForm>
       </section>
     </>
